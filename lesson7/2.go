8fcc19ba6c6f3572ff2cbfd834e5459629e52bbc
Irina <irina@example.com>
2025-06-21 21:30:59 +0300

    commit_lesson7_2.6

--- lesson7/2.go
+++ lesson7/2.go
@@ -1,33 +1,26 @@
-/*package main
-
-import "fmt"
-
-
-func main() {
-	//error type
-	err := fmt.Errorf("I am error!")
-	fmt.Println(err)
-
-} */
-
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-func myFuncReturnsError() (string, error) {
-	return "I am String!", errors.New("I am errors error")
+func div(a, b int) (int, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("Div by 0!")
+	}
+	return a / b, nil
+
 }
+
 func main() {
-	//error type
 
-	myString, _ := myFuncReturnsError() //забили на ошибку
-	/*	if myError != nil {
-		fmt.Println(myError)
-	} */
+	a, b := 2, 1
+	result, err := div(a, b)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	fmt.Println(myString)
+	fmt.Println(result)
 
 }
