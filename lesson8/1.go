0548924a4365760b434a5aadef25f3571dd56455
Irina <irina@example.com>
2025-06-22 10:49:33 +0300

    lesson8_1.2_массив

--- lesson8/1.go
+++ lesson8/1.go
@@ -1,8 +1,32 @@
-package main
+/*package main
 
 import "fmt"
 
 func main() {
-	temp := 36.6
-	fmt.Println("Temperature: " + fmt.Sprintf("%v", temp))
+	var planets [8]string
+	planets[0] = "Меркурий"
+	planets[1] = "Венера"
+	planets[2] = "Земля"
+
+	earth := planets[2]
+	fmt.Println(planets)
+	fmt.Println("Наша планета: ", earth)
+
+	fmt.Println(len(planets))
+	fmt.Println(planets[5] == "")
+}
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var planets [8]string
+	i := 7
+
+	fmt.Println(planets[i])
+
 }
