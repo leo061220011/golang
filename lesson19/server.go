0b656035b1d74fde939d9ab999aeece27d16d855
Irina <irina@example.com>
2025-07-17 21:08:10 +0300

    server.go_1

--- lesson19/server.go
+++ lesson19/server.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -18,7 +19,7 @@ POST /books - отправить (создать) новую книгу
 */
 
 type Book struct {
-	ID     uint
+	ID     int
 	Title  string
 	Author string
 }
@@ -31,7 +32,21 @@ func BooksHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func AddBookHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Add Book")
+	//	fmt.Fprint(w, "Add Book")
+	params := mux.Vars(r)
+	//fmt.Fprint(w, params["id"])
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, value := range books {
+		if value.ID == id {
+			json.NewEncoder(w).Encode(value)
+			return
+
+		}
+	}
+	json.NewEncoder(w).Encode(Book{})
 }
 
 func main() {
@@ -44,3 +59,55 @@ func main() {
 	http.ListenAndServe("127.0.0.1:8080", r)
 
 }
+
+// //*
+
+// package main
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"net/http"
+// 	"strconv"
+// 	"github.com/gorilla/mux"
+// )
+// /*
+// GET /books - получить все книги
+// GET /books/{id} - получить одну книгу с идентификатором
+// POST /books - отправить (создать) новую книгу
+// */
+// type Book struct {
+// 	ID     int
+// 	Title  string
+// 	Author string
+// }
+// var books = []Book{}
+// func BooksHandler(w http.ResponseWriter, r *http.Request) {
+// 	// fmt.Fprint(w, books)
+// 	json.NewEncoder(w).Encode(books)
+// }
+// func AddBookHandler(w http.ResponseWriter, r *http.Request) {
+// 	params := mux.Vars(r) //достать все параметры из url
+// 	// fmt.Fprint(w, params["id"])
+// 	id, err := strconv.Atoi(params["id"]) //перевожу параметр id в число (тк в структуре у меня ID - число)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	for _, value := range books { //иду по слайсу books и сравниваю параметр id с идентификатором книги
+// 		if value.ID == id {
+// 			json.NewEncoder(w).Encode(value)
+// 			return
+// 		}
+// 	}
+// 	json.NewEncoder(w).Encode(Book{}) //если не найден - вывожу пустую структуру книги
+// }
+// func main() {
+// 	books = append(books, Book{ID: 1, Title: "Искусство любить", Author: "Эрих Фромм"})
+// 	books = append(books, Book{ID: 2, Title: "Война и Мир", Author: "Толстой"})
+// 	r := mux.NewRouter()
+// 	r.HandleFunc("/books", BooksHandler)
+// 	r.HandleFunc("/books/{id}", AddBookHandler)
+// 	fmt.Println("СТАРТУЕМ! Я сказал стартуем...")
+// 	http.ListenAndServe("127.0.0.1:8080", r)
+// }
+// *//
