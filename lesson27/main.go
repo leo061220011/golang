7e372a8a56b01d2e05490cbb74fad77bdbfd329b
Irina <irina@example.com>
2025-08-11 20:25:06 +0300

    lesson27

--- lesson27/main.go
+++ lesson27/main.go
@@ -8,15 +8,29 @@ import (
 
 func main() {
 	r := gin.Default()
-
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello from Gin Gonic Go!")
-
+		c.JSON(http.StatusOK, gin.H{
+			"Name": "Sergey",
+			"Age":  66,
+		})
 	})
 	r.GET("/user/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.String(http.StatusOK, "Hello, %s!", name)
-
+	})
+	r.POST("/user", func(c *gin.Context) {
+		type User struct {
+			Name  string
+			Email string
+		}
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"Status": "Ok!",
+		})
 	})
 	r.Run(":8080")
 }
